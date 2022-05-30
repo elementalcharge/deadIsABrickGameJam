88a90d8eecf0ac19d91b6030e634afbedc4b1bea
German Daniel Boso <german.boso@gmail.com>
2022-05-29 23:16:43 -0300

    commit of what I worked during the jam
    I should have done this sooner not now...

---
+++ Assets/ReSpawnController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReSpawnController : MonoBehaviour
+{
+    [SerializeField] Transform SpawnPosition;
+    [SerializeField] GameObject player;
+    [SerializeField] GameObject brick;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Instantiate(brick, player.transform.TransformPoint(Vector3.zero), player.transform.rotation );
+            player.transform.SetPositionAndRotation( SpawnPosition.position, transform.rotation);
+        }
+        
+    }
+}
